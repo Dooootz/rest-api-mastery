9b0bb5e581fa93b369f898766a141042dc00b7c5
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-02-17 14:19:29 +1000

    GET request successful

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+// import express & bodyParser
 import express from 'express';
+// bodyParser allows us to parse requests & response bodies as JSON
 import bodyParser from 'body-parser';
 
+// create express app = app
 const app = express();
+// instatiate PORT number
 const PORT = 5000;
 
+// use bodyParser to parse req & res bodies as JSON 
 app.use(bodyParser.json());
 
-app.listen(PORT, () => {console.log(`ITS ALIVE!!! on http://localhost:${PORT}`)})
+// create route - we are expecting a GET request
+// first parameter is the PATH or endpoint
+// second parameter is a callback function that expects 2 parameters - req & res 
+app.get('/', (req, res) => {
+    console.log('[TEST]!')
+
+    res.send('Hello from the matrix')
+})
+
+app.listen(PORT, () => {console.log(`ITS ALIVE!!! on http://localhost:${PORT}`)}) 
