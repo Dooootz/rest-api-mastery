8847141e2c4dec77f0c84817c8d44fc935500051
Tre Molony <78519383+Dooootz@users.noreply.github.com>
2022-02-17 15:07:02 +1000

    POST request successful

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import express from 'express';
 // bodyParser allows us to parse requests & response bodies as JSON
 import bodyParser from 'body-parser';
 
+import usersRoutes from './routes/users.js'
+
 // create express app = app
 const app = express();
 // instatiate PORT number
@@ -11,13 +13,17 @@ const PORT = 5000;
 // use bodyParser to parse req & res bodies as JSON 
 app.use(bodyParser.json());
 
+app.use('/users', usersRoutes)
+
 // create route - we are expecting a GET request
 // first parameter is the PATH or endpoint
 // second parameter is a callback function that expects 2 parameters - req & res 
 app.get('/', (req, res) => {
     console.log('[TEST]!')
-
+    // send message
     res.send('Hello from the matrix')
 })
 
+// listen on PORT... then 
+// callback function which prints URL to console 
 app.listen(PORT, () => {console.log(`ITS ALIVE!!! on http://localhost:${PORT}`)}) 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,45 @@
+// import express...
 import express from "express";
+// uuid generates random endpoints in the URI
+import { v4 as uuidv4 } from 'uuid';
 
+// create Router
 const router = express.Router();
 
-router.get('/users', (req, res) => {
-    res.send('Howdy')
+// mock db
+const users = [
+    {
+        firstName:"John",
+        lastName:"Doe",
+        age:"25"
+    },
+    {
+        firstName:"Wa",
+        lastName:"Wawewa",
+        age:"23"
+    }
+]
+
+// all routes here start with '/users'
+router.get('/', (req, res) => {
+    console.log(users)
+    res.send(users)
 })
 
+// POST method on /users endpoint
+router.post('/', (req, res) => {
+    // user = entire request body
+    const user = req.body
+
+    // const userId = uuidv4();
+    // const userWithId = { ...user, id: userId }
+    // users.push(userWithId);
+    // --- refactored code below ---
+    users.push({ ...user, id: uuidv4() });
+
+    // response send message
+    res.send(`Player: ${user.firstName} ${user.lastName} added to the matrix`)
+})
+
+// export this router
 export default router
